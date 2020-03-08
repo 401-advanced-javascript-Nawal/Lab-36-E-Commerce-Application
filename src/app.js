6eddeacf403035946f081103dcf7bd16002f802f
Nsuliman <nsuliman88@gmail.com>
2020-03-08 15:21:28 +0200

    footer and header done

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
 import React from 'react';
+import Header from './modules/Header.js';
+import Footer from './modules/Footer.js';
+
+// import style if exist 
+
+function App(){
+    return (
+        <>
+        <Header />
+        <Footer />
+        </>
+    )
+}
+
+export default App;
