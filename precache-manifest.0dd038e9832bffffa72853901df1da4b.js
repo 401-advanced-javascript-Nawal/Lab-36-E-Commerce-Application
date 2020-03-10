6486d983e5e44901e97cae8d04dbb88b7405fb33
Nsuliman <runner@fv-az117.g5eyvibpvwqutjohurqvnll3ig.bx.internal.cloudapp.net>
2020-03-10 11:41:15 +0000

    Updates

---
+++ precache-manifest.0dd038e9832bffffa72853901df1da4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d81b7e59bce95e6121bf08865511a23",
+    "url": "/Lab-36-E-Commerce-Application/index.html"
+  },
+  {
+    "revision": "5c13d28bcd9702580106",
+    "url": "/Lab-36-E-Commerce-Application/static/css/main.b01c927f.chunk.css"
+  },
+  {
+    "revision": "8817b1d10da471e49dac",
+    "url": "/Lab-36-E-Commerce-Application/static/js/2.7aaa2e80.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/Lab-36-E-Commerce-Application/static/js/2.7aaa2e80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c13d28bcd9702580106",
+    "url": "/Lab-36-E-Commerce-Application/static/js/main.a8aa9025.chunk.js"
+  },
+  {
+    "revision": "536002480fdfa22b52c4",
+    "url": "/Lab-36-E-Commerce-Application/static/js/runtime-main.f427c5d8.js"
+  }
+]);
